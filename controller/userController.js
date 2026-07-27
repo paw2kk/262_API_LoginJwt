445003ee5f3b7167ce53f64fd0d32cf110fa66ad
paw2k <wndxakk@gmail.com>
2026-07-27 16:13:16 +0700

    fix: correct error handling in register function and ensure password comparison in login function

--- controller/userController.js
+++ controller/userController.js
@@ -25,7 +25,7 @@ async function register(req, res) {
         return res.status(201).json({ message: "User registered successfully.", data : { id: user.id, email: user.email } });
 
     } catch (err) {
-        return res.status(500).json({ message: error.message });
+        return res.status(500).json({ message: err.message });
     }
 }
 
@@ -42,6 +42,8 @@ async function login(req, res) {
         if (!user) {
             return res.status(400).json({ message: 'Invalid email or password.' });
         }
+
+        const isMatch = await bcrypt.compare(password, user.password);
 
         if (!isMatch) {
             return res.status(400).json({ message: 'Invalid email or password.' });
