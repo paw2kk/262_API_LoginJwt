fb56d0dc420cc9c87d2bbe20ddaa62dfab5aafdb
paw2k <wndxakk@gmail.com>
2026-07-27 15:27:29 +0700

    feat: complete user registration logic in userController

--- controller/userController.js
+++ controller/userController.js
@@ -15,4 +15,13 @@ async function registerUser(req, res) {
 
         if (existingUser) {
             return res.status(400).json({ message: 'Email already exists.' });
-            
+        }
+        
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const user = await User.create({ email, password: hashedPassword });
+
+        return res.status(201).json({ message: "User registered successfully.", data : { id: user.id, email: user.email } });
+
+    } catch (err) {
+        return res.status(500).json({ message: error.message });
